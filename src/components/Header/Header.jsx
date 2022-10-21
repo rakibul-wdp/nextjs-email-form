393fe0954bbae944d7e047117f825c4502b6b93b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-22 04:05:00 +0600

    create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderStyles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={HeaderStyles.header}>To Do App</div>
+  )
+}
+
+export default Header;
