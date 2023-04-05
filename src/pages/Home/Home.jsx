3608b65016db237a811498a8c1663d2ae5e003d7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-05 21:09:46 +0600

    create home page with just boilarplate

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeStyles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <div className={HomeStyles.home}>Home</div>
+  )
+}
+
+export default Home;
