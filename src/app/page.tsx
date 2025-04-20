6568e7d13415e5bbd1a23db710767f4edc1bbed9
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-20 17:20:48 +0600

    change style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "@/components/ContactForm";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-start justify-center px-4">
+    <div className="min-h-screen flex items-start justify-center px-4 mt-10">
       <div className="w-full max-w-md bg-white dark:bg-zinc-900 shadow-lg rounded-2xl p-8 space-y-6 border dark:border-zinc-800">
         <h2 className="text-2xl font-semibold text-center text-zinc-900 dark:text-white">
           Contact Us
