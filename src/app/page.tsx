abb6d12183b30a9217d3561b25aad0496f7d3ab0
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-20 16:30:09 +0600

    email js functionality added and it's work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
 export default function Home() {
   const [userInput, setUserInput] = useState({
@@ -22,7 +24,40 @@ export default function Home() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(userInput.name, userInput.email, userInput.message);
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    try {
+      const emailParams = {
+        name: userInput.name,
+        email: userInput.email,
+        message: userInput.message,
+      };
+
+      if (!serviceID || !templateID || !userID) {
+        throw new Error("Missing EmailJS environment variables.");
+      }
+
+      const res = await emailjs.send(
+        serviceID,
+        templateID,
+        emailParams,
+        userID
+      );
+
+      if (res.status === 200) {
+        toast.success("Message sent successfully!");
+        setUserInput({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to send message. Please try again later.");
+    }
   };
 
   return (
@@ -56,7 +91,9 @@ export default function Home() {
           required
         />
       </div>
-      <button type="submit">Send Message</button>
+      <button type="submit" className="cursor-pointer">
+        Send Message
+      </button>
     </form>
   );
 }
