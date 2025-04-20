4fbc57416b1cffd5aa278342635f3b58803745fe
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-20 16:43:06 +0600

    separate submit button

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,10 @@
+export default function SubmitButton() {
+  return (
+    <button
+      type="submit"
+      className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+    >
+      Send Message
+    </button>
+  );
+}
